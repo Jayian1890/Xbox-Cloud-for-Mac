5c6aa4d13a5343ddc5025cd3557b04ee3c4cb6b7
Jared <jaredt741@gmail.com>
2023-02-05 20:37:15 -0600

    Initial Commit

---
+++ xCloud/xCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  xCloudApp.swift
+//  xCloud
+//
+//  Created by Jared Terrance on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct xCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
