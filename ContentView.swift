358dad634d5fb725205dbe597d5759d659beed35
Jared <jaredt741@gmail.com>
2023-02-22 09:15:18 -0600

    organize code

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 /// The main View for displaying and handling the primary app window.
 struct ContentView: View {
@@ -24,68 +23,22 @@ struct ContentView: View {
         WebView(data: WebViewData(url: self.url!, customUserAgent: self.userAgent))
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
-                    Button(action: { record() }) {
+                    Button(action: {
+                        Task {
+                            await contentRecorder.RecordVideo()
+                        }
+                    }) {
                         Label("Record", systemImage: "video.circle").labelStyle(.titleAndIcon)
                     }.keyboardShortcut(KeyEquivalent("r"), modifiers: .command)
                 }
                 ToolbarItem(placement: .primaryAction) {
-                    Button(action: { takeSnapshot() }) {
+                    Button(action: {
+                        Screenshot().takeSnapshot(webview: WebViewCoordinator.WebView)
+                    }) {
                         Label("Screenshot", systemImage: "photo.circle").labelStyle(.titleAndIcon)
                     }.keyboardShortcut(KeyEquivalent("s"), modifiers: .command)
                 }
             }
-    }
-    
-    /// Captures video frames of the current content within View
-    /// - warning:  Not imlpemented
-    func record() {
-        Task {
-            await contentRecorder.RecordVideo()
-        }
-    }
-    
-    /// Take a snapshot of the current WKWebView
-    func takeSnapshot() {
-        let config = WKSnapshotConfiguration()
-        WebViewCoordinator.WebView.takeSnapshot(with: config) { image, error in
-            if let error = error {
-                print("Error taking snapshot: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let image = image else {
-                print("Error taking snapshot: no image returned")
-                return
-            }
-            
-            Media.Screenshot()
-            saveImage(image: image)
-        }
-    }
-    
-    /// Saves a provided image to the default Downloads directory on the system
-    func saveImage(image: NSImage) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyyMMdd_HHmmss"
-        let dateString = dateFormatter.string(from: Date())
-        
-        if let imageData = image.tiffRepresentation,
-           let bitmapImage = NSBitmapImageRep(data: imageData),
-           let jpegData = bitmapImage.representation(using: .jpeg, properties: [:]) {
-            
-            let paths = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)
-            let directory = paths[0]
-            
-            let filename = "\(dateString).jpg"
-            let fileURL = directory.appendingPathComponent(filename)
-            
-            do {
-                try jpegData.write(to: fileURL)
-                print("Saved image: \(directory)\(filename)")
-            } catch {
-                print("Error saving image: \(error.localizedDescription)")
-            }
-        }
     }
 
 }
---
+++ Screenshot.swift
@@ -0,0 +1,67 @@
+//
+//  Screenshot.swift
+//  Xbox Cloud
+//
+//  Created by Jared T on 2/22/23.
+//
+
+import WebKit
+
+/// This class contains functions for taking and saving screenshots of the internal application
+class Screenshot {
+    
+    /// contains configuration information for the webkit snapshot
+    private let config = WKSnapshotConfiguration()
+    
+    /// This variable caches the most recent image taken by this class
+    private var lastImage: NSImage? = nil
+    
+    init() {
+        config.afterScreenUpdates = true
+    }
+    
+    /// Take a snapshot of the the supplied WKWebView
+    func takeSnapshot(webview: WKWebView) {
+        webview.takeSnapshot(with: config) { image, error in
+            if let error = error {
+                print("Error taking snapshot: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let image = image else {
+                print("Error taking snapshot: no image returned")
+                return
+            }
+            
+            Media.Screenshot()
+            self.saveImage(image: image)
+            
+            self.lastImage = image
+        }
+    }
+    
+    /// Saves a provided image to the default Downloads directory on the system
+    private func saveImage(image: NSImage) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd_HHmmss"
+        let dateString = dateFormatter.string(from: Date())
+        
+        if let imageData = image.tiffRepresentation,
+           let bitmapImage = NSBitmapImageRep(data: imageData),
+           let jpegData = bitmapImage.representation(using: .jpeg, properties: [:]) {
+            
+            let paths = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)
+            let directory = paths[0]
+            
+            let filename = "\(dateString).jpg"
+            let fileURL = directory.appendingPathComponent(filename)
+            
+            do {
+                try jpegData.write(to: fileURL)
+                print("Saved image: \(directory)\(filename)")
+            } catch {
+                print("Error saving image: \(error.localizedDescription)")
+            }
+        }
+    }
+}
